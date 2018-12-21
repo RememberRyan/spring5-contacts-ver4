d22902aafcb7ded5961c5f6c0d41660ecadfcb8b
Ryan <hiRyan@protonmail.com>
2018-12-21 19:36:50 +0200

    Changes to the SQL intialisation files

--- src/main/resources/db/migration/V1_2_0__data.sql
+++ src/main/resources/db/migration/V1_2_0__data.sql
@@ -1,6 +1,6 @@
-INSERT INTO contact (id, name, last_name, creation_time, status, description) VALUES (1, 'First', 'User', '2018-11-30 19:07:29', 'ACTIVE', null);
-INSERT INTO contact (id, name, last_name, creation_time, status, description) VALUES (2, 'Alex', 'Beljakov', '2018-11-30 19:11:21', 'ACTIVE', null);
-INSERT INTO contact (id, name, last_name, creation_time, status, description) VALUES (3, 'Test', 'Test', '2018-11-30 19:11:33', 'ACTIVE', null);
-INSERT INTO contact (id, name, last_name, creation_time, status, description) VALUES (4, 'Den', 'T', '2018-11-30 19:11:40', 'ACTIVE', null);
+INSERT INTO contact (id, name, last_name, creation_time, status, description) VALUES (1, 'Don', 'Draper', '2018-12-20 19:07:29', 'ACTIVE', null);
+INSERT INTO contact (id, name, last_name, creation_time, status, description) VALUES (2, 'John', 'Barnes', '2018-11-30 19:11:21', 'ACTIVE', null);
+INSERT INTO contact (id, name, last_name, creation_time, status, description) VALUES (3, 'Anna', 'Kournikova', '2018-11-30 19:11:33', 'ACTIVE', null);
+INSERT INTO contact (id, name, last_name, creation_time, status, description) VALUES (4, 'August', 'Rayne', '2018-11-30 19:11:40', 'ACTIVE', null);
 
-INSERT INTO phone (id, contact_id, country_code, phone_number, description, type) VALUES (1, 1, 372, 54333445, 'Here is my phone number', 'HOME');
+INSERT INTO phone (id, contact_id, country_code, phone_number, description, type) VALUES (1, 1, 372, 52333445, 'Black book', 'HOME');
